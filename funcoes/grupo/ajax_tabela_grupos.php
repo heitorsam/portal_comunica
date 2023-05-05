1e4dee43fe5213209aa1a8b0f966ce83d3f1b3ae
Rafael Yasser <rafael.yasser20@gmail.com>
2023-05-05 09:57:16 -0300

    ajustes grupo

--- funcoes/grupo/ajax_tabela_grupos.php
+++ funcoes/grupo/ajax_tabela_grupos.php
@@ -8,11 +8,17 @@
 
     // BUSCA APENAS OS GRUPOS QUE PERTENCEM A EMPRESA DO USUÁRIO QUE ESTÁ LOGADO
     $cons_buscar_grupos = "SELECT CD_GRUPO,                           
-                           DS_GRUPO,                           
-                           (SELECT COUNT(CD_CHAMADO) FROM portal_comunica.CHAMADO WHERE CD_GRUPO = grp.CD_GRUPO) AS QTD_CHAMADOS
-                           FROM portal_comunica.GRUPO grp
-                           WHERE grp.CD_EMPRESA = $cd_empresa_usuario_logado
-                           ORDER BY CD_GRUPO DESC";
+                                DS_GRUPO,                           
+                                (SELECT COUNT(CD_CHAMADO) 
+                                FROM portal_comunica.CHAMADO ch
+                                WHERE CD_GRUPO = grp.CD_GRUPO
+                                    AND ch.TP_STATUS IN ('A', 'E')) AS QTD_CHAMADOS,
+                                (SELECT COUNT(CD_USUARIO)
+                                FROM portal_comunica.GRUPO_USUARIO grpusu
+                                WHERE grpusu.CD_GRUPO = grp.CD_GRUPO) AS QTD_USUARIOS
+                            FROM portal_comunica.GRUPO grp
+                            WHERE grp.CD_EMPRESA = $cd_empresa_usuario_logado
+                            ORDER BY CD_GRUPO DESC";
 
     $res = mysqli_query($conn, $cons_buscar_grupos);
 
@@ -25,6 +31,7 @@
         <th class="p-2" style="text-align: center; white-space: nowrap;">Código</th>
         <th class="p-2" style="text-align: center; white-space: nowrap;">Grupo</th>
         <th class="p-2" style="text-align: center; white-space: nowrap;">Chamados</th>
+        <th class="p-2" style="text-align: center; white-space: nowrap;">Usuários</th>
         <th class="p-2" style="text-align: center; white-space: nowrap;">Opções</th>
 
     </thead>
@@ -40,11 +47,12 @@
                     echo '<td class="align-middle">'. $row['CD_GRUPO'] .'</td>';
                     echo '<td class="align-middle">'. $row['DS_GRUPO'] .'</td>';
                     echo '<td class="align-middle">'. $row['QTD_CHAMADOS'] .'</td>';
+                    echo '<td class="align-middle">'. $row['QTD_USUARIOS'] .'</td>';
                     echo '<td class="align-middle">
                         <button onclick="ajax_chamar_modal_edicao('. $row['CD_GRUPO'] .')" class="btn btn-primary"><i class="fa-solid fa-pencil"></i></button>
                         <button onclick="ajax_chamar_modal_membro('. $row['CD_GRUPO'] .')" class="btn btn-primary"><i class="fa-solid fa-people-group"></i></button>';
 
-                        if($row['QTD_CHAMADOS'] == 0){
+                        if($row['QTD_CHAMADOS'] == 0 && $row['QTD_USUARIOS'] == 0){
         ?>
 
                             <button onclick="ajax_alert('Deseja excluir grupo?','excluir_grupo(<?php echo $row['CD_GRUPO']; ?>)')" class="btn btn-adm"><i class="fa-solid fa-trash-can"></i></button>
